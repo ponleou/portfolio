2e90c6b913783a51920c7dd768c5235b926c7325
Keo Ponleou Sok <ponleousk@gmail.com>
2025-06-15 18:08:07 +1000

    fix: formatting

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,23 +2,15 @@ import { useEffect, useRef, useState } from "react";
 import GoldenHorizontal from "./components/GoldenHorizontal";
 
 export default function Home() {
-  useEffect(() => {}, []);
+    useEffect(() => {}, []);
 
-  return (
-    <div className="h-dvh bg-bg">
-      <GoldenHorizontal
-        className="h-dvh"
-        top={
-          <h1 className="text-primary text-h1 font-bold leading-[0.85] text-center">
-            Keo Ponleou
-          </h1>
-        }
-        bottom={
-          <div className="text-primary text-h3 font-bold text-center">
-            SOFTWARE DEVELOPER
-          </div>
-        }
-      />
-    </div>
-  );
+    return (
+        <div className="h-dvh bg-bg">
+            <GoldenHorizontal
+                className="h-dvh"
+                top={<h1 className="text-primary text-h1 font-bold leading-[0.85] text-center">Keo Ponleou</h1>}
+                bottom={<div className="text-primary text-h3 font-bold text-center">SOFTWARE DEVELOPER</div>}
+            />
+        </div>
+    );
 }
