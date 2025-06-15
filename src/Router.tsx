68b0303fba4d08c275bcd76a9204219228cb4669
Keo Ponleou Sok <ponleousk@gmail.com>
2025-06-15 18:08:07 +1000

    fix: formatting

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,16 +2,14 @@ import { Route } from "react-router";
 import { BrowserRouter, Routes } from "react-router";
 import Home from "./Home";
 
-
 function Router() {
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default Router
+export default Router;
