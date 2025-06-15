5d5ff82218817d652c7842728a53ed5d67a06c42
Keo Ponleou Sok <ponleousk@gmail.com>
2025-06-15 18:48:34 +1000

    feat: added textcycle component and used typewriter function

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import GoldenHorizontal from "./components/GoldenHorizontal";
+import TextCycle from "./components/TextCycle";
+import typewriter from "./functions/typewriter";
 
 export default function Home() {
     useEffect(() => {}, []);
@@ -8,8 +10,18 @@ export default function Home() {
         <div className="h-dvh bg-bg">
             <GoldenHorizontal
                 className="h-dvh"
-                top={<h1 className="text-primary text-h1 font-bold leading-[0.85] text-center">Keo Ponleou</h1>}
-                bottom={<div className="text-primary text-h3 font-bold text-center">SOFTWARE DEVELOPER</div>}
+                top={<h1 className="text-primary text-h1 font-extrabold leading-[0.85] text-center">Keo Ponleou</h1>}
+                bottom={
+                    <div className="text-primary text-h3 font-semibold text-center">
+                        SOFTWARE DEVELOPER
+                        <TextCycle
+                            textArray={["React.js", "HTML"]}
+                            changeTime={100}
+                            pauseTime={1500}
+                            callback={typewriter}
+                        />
+                    </div>
+                }
             />
         </div>
     );
