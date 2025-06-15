40c0982fa9eb9155db20fc84af950c4678306761
Keo Ponleou Sok <ponleousk@gmail.com>
2025-06-15 18:08:20 +1000

    feat: typewriter function

---
+++ src/functions/typewriter.ts
@@ -0,0 +1,30 @@
+export default function Typewriter(current: string, text: string): [boolean, string] {
+    if (current === text) {
+        return [true, current];
+    }
+
+    let isSubset = true;
+    let returnString = current;
+
+    for (let i = 0; i < returnString.length; i++) {
+        if (i >= text.length) {
+            isSubset = false;
+            break;
+        }
+
+        if (returnString[i] === text[i]) {
+            continue;
+        } else {
+            isSubset = false;
+            break;
+        }
+    }
+
+    if (isSubset) {
+        returnString += text[returnString.length];
+    } else {
+        returnString = returnString.slice(0, -1);
+    }
+
+    return [returnString === text, returnString];
+}
