35924169b42d296e22dd0520371e62964490b20a
Keo Ponleou Sok <ponleousk@gmail.com>
2025-06-15 18:08:07 +1000

    fix: formatting

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import Router from "./Router.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  // <StrictMode>
-  <Router />
-  // </StrictMode>,
+    // <StrictMode>
+    <Router />
+    // </StrictMode>,
 );
