87ac46cc4d44193e7d518083f0b9c99379483cf8
Keo Ponleou Sok <ponleousk@gmail.com>
2025-06-16 21:16:40 +1000

    fix: makes element align more consistently

--- src/components/VerticalAlignTarget.tsx
+++ src/components/VerticalAlignTarget.tsx
@@ -3,15 +3,23 @@ import { useEffect, useRef, useState, type ReactNode } from "react";
 export default function VerticalAlignTarget({ element, children }: { element: HTMLDivElement; children: ReactNode }) {
     const parent = useRef<HTMLDivElement>(null);
 
+    function align() {
+        parent.current!.style.left = `${element.getBoundingClientRect().left}px`;
+    }
+
     useEffect(() => {
-        const observer = new ResizeObserver(() => {
-            parent.current!.style.left = `${element.getBoundingClientRect().left}px`;
-        });
+        const observer = new ResizeObserver(align);
 
-        if (parent.current) observer.observe(parent.current);
+        if (parent.current) {
+            observer.observe(parent.current);
+            window.addEventListener("resize", align);
+        }
 
-        return () => observer.disconnect();
-    }, [element.getBoundingClientRect()]);
+        return () => {
+            observer.disconnect();
+            window.removeEventListener("resize", align);
+        };
+    }, []);
 
     return (
         <div ref={parent} className={`absolute`}>
