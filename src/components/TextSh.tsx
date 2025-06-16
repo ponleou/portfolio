f2d8879cfeab1874d7a8523780cb1e63ed05fc14
Keo Ponleou Sok <ponleousk@gmail.com>
2025-06-16 20:57:05 +1000

    feat: created navbar component

---
+++ src/components/TextSh.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from "react";
+
+export default function TextSh({ children, className }: { children: ReactNode; className: string }) {
+    return (
+        <div className={`flex ${className}`}>
+            <p>./</p>
+            <p>{children}</p>
+            <p>.sh</p>
+        </div>
+    );
+}
