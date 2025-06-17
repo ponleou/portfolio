4847d28f3aa50b3b9480205323b4909875f97121
Keo Ponleou Sok <ponleousk@gmail.com>
2025-06-17 21:08:35 +1000

    feat: added enqueued reveal on elements

--- src/components/RevealText.tsx
+++ src/components/RevealText.tsx
@@ -8,39 +8,43 @@ export default function RevealText({
     delayPerCallback,
     revealCallback,
     finishedCallback,
+    startOn = true,
 }: {
     initialText?: string;
     text: string;
     delayPerCallback: number;
     revealCallback: RevealFunction;
     finishedCallback?: (finished: boolean) => void;
+    startOn?: boolean;
 }) {
     const [textDisplay, setTextDisplay] = useState<string>(initialText);
 
     useEffect(() => {
-        let finished = false;
-        let currentText = textDisplay;
+        if (startOn) {
+            let finished = false;
+            let currentText = textDisplay;
 
-        (async () => {
-            while (!finished) {
-                if (currentText === "") {
-                    await delay(delayPerCallback);
+            (async () => {
+                while (!finished) {
+                    if (currentText === "") {
+                        await delay(delayPerCallback);
+                    }
+
+                    const [isFinished, returnText] = revealCallback(currentText, text);
+                    finished = isFinished;
+                    currentText = returnText;
+                    setTextDisplay(currentText);
+
+                    if (!finished) {
+                        await delay(delayPerCallback);
+                    }
                 }
-
-                const [isFinished, returnText] = revealCallback(currentText, text);
-                finished = isFinished;
-                currentText = returnText;
-                setTextDisplay(currentText);
-
-                if (!finished) {
-                    await delay(delayPerCallback);
+                if (finishedCallback) {
+                    finishedCallback(true);
                 }
-            }
-            if (finishedCallback) {
-                finishedCallback(true);
-            }
-        })();
-    }, [text, initialText]);
+            })();
+        }
+    }, [text, initialText, startOn]);
 
-    return <p>{textDisplay}</p>;
+    return <p className={`h-[1lh]`}>{textDisplay}</p>;
 }
