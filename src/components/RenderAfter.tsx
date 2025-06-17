c6b1148bfdb64cd97ccdc088317d6d1fcfb0b45b
Keo Ponleou Sok <ponleousk@gmail.com>
2025-06-17 15:09:22 +1000

    feat: created a renderAfter component to render things after an element

---
+++ src/components/RenderAfter.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState, type ReactNode } from "react";
+
+export default function RenderAfter({
+    after,
+    children,
+    rerenderCallback,
+}: {
+    after: HTMLDivElement | null;
+    children: ReactNode;
+    rerenderCallback: () => void;
+}) {
+    const [render, setRender] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (after) {
+            setRender(true);
+        } else {
+            rerenderCallback();
+        }
+    }, [after]);
+
+    return <>{render && children}</>;
+}
