baddf449aaeee7cf48ae016bf6b8d1b2be605400
Keo Ponleou Sok <ponleousk@gmail.com>
2025-06-19 18:04:27 +1000

    feat: added reactive component to cursor movement

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,35 @@
 import { NavLink } from "react-router";
 import TextSh from "./TextSh";
+import TranslateToCursor from "./TranslateToCursor";
 
 export default function NavBar({ className = "", navClassName = "" }: { className?: string; navClassName?: string }) {
     return (
         <div className={`${className} text-accent text-base font-bold`}>
-            <NavLink to="/about">
-                <TextSh className={`${navClassName}`}>about</TextSh>
-            </NavLink>
-            <NavLink to="/experience">
-                <TextSh className={`${navClassName}`}>experience</TextSh>
-            </NavLink>
-            <NavLink to="/projects">
-                <TextSh className={`${navClassName}`}>projects</TextSh>
-            </NavLink>
-            <NavLink to="/skills">
-                <TextSh className={`${navClassName}`}>skills</TextSh>
-            </NavLink>
-            <NavLink to="/contact">
-                <TextSh className={`${navClassName}`}>contact</TextSh>
-            </NavLink>
+            <TranslateToCursor maxTranslate={2} translateMultiplier={0.1}>
+                <NavLink to="/about">
+                    <TextSh className={`${navClassName}`}>about</TextSh>
+                </NavLink>
+            </TranslateToCursor>
+            <TranslateToCursor maxTranslate={2} translateMultiplier={0.1}>
+                <NavLink to="/experience">
+                    <TextSh className={`${navClassName}`}>experience</TextSh>
+                </NavLink>
+            </TranslateToCursor>
+            <TranslateToCursor maxTranslate={2} translateMultiplier={0.1}>
+                <NavLink to="/projects">
+                    <TextSh className={`${navClassName}`}>projects</TextSh>
+                </NavLink>
+            </TranslateToCursor>
+            <TranslateToCursor maxTranslate={2} translateMultiplier={0.1}>
+                <NavLink to="/skills">
+                    <TextSh className={`${navClassName}`}>skills</TextSh>
+                </NavLink>
+            </TranslateToCursor>
+            <TranslateToCursor maxTranslate={2} translateMultiplier={0.1}>
+                <NavLink to="/contact">
+                    <TextSh className={`${navClassName}`}>contact</TextSh>
+                </NavLink>
+            </TranslateToCursor>
         </div>
     );
 }
