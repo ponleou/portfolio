b1e15730b20acb1259b23d8bb764fa5ea0a38e38
Keo Ponleou Sok <ponleousk@gmail.com>
2025-06-20 12:19:05 +1000

    fix: remove movement from navbar buttons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,31 +5,21 @@ import TranslateToCursor from "./TranslateToCursor";
 export default function NavBar({ className = "", navClassName = "" }: { className?: string; navClassName?: string }) {
     return (
         <div className={`${className} text-accent text-base font-bold`}>
-            <TranslateToCursor maxTranslate={2} translateMultiplier={0.1}>
-                <NavLink to="/about">
-                    <TextSh className={`${navClassName}`}>about</TextSh>
-                </NavLink>
-            </TranslateToCursor>
-            <TranslateToCursor maxTranslate={2} translateMultiplier={0.1}>
-                <NavLink to="/experience">
-                    <TextSh className={`${navClassName}`}>experience</TextSh>
-                </NavLink>
-            </TranslateToCursor>
-            <TranslateToCursor maxTranslate={2} translateMultiplier={0.1}>
-                <NavLink to="/projects">
-                    <TextSh className={`${navClassName}`}>projects</TextSh>
-                </NavLink>
-            </TranslateToCursor>
-            <TranslateToCursor maxTranslate={2} translateMultiplier={0.1}>
-                <NavLink to="/skills">
-                    <TextSh className={`${navClassName}`}>skills</TextSh>
-                </NavLink>
-            </TranslateToCursor>
-            <TranslateToCursor maxTranslate={2} translateMultiplier={0.1}>
-                <NavLink to="/contact">
-                    <TextSh className={`${navClassName}`}>contact</TextSh>
-                </NavLink>
-            </TranslateToCursor>
+            <NavLink to="/about">
+                <TextSh className={`${navClassName}`}>about</TextSh>
+            </NavLink>
+            <NavLink to="/experience">
+                <TextSh className={`${navClassName}`}>experience</TextSh>
+            </NavLink>
+            <NavLink to="/projects">
+                <TextSh className={`${navClassName}`}>projects</TextSh>
+            </NavLink>
+            <NavLink to="/skills">
+                <TextSh className={`${navClassName}`}>skills</TextSh>
+            </NavLink>
+            <NavLink to="/contact">
+                <TextSh className={`${navClassName}`}>contact</TextSh>
+            </NavLink>
         </div>
     );
 }
