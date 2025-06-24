f7d4d12445624ef7647f59dea1ef4d3c4fc28f9e
Keo Ponleou Sok <ponleousk@gmail.com>
2025-06-24 20:45:57 +1000

    feat: added parralax on elements

---
+++ src/components/TranslateOnScroll.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useRef, type ReactNode } from "react";
+
+const directions = ["vertical", "horizontal"] as const;
+type Direction = (typeof directions)[number];
+
+export default function TranslateOnScroll({
+    children,
+    direction,
+    rate,
+    maxScroll,
+}: {
+    children: ReactNode;
+    direction: Direction;
+    rate: number;
+    maxScroll: number;
+}) {
+    const parent = useRef<HTMLDivElement>(null);
+
+    function translateElement(element: HTMLDivElement, direction: Direction, translate: number) {
+        if (direction === "horizontal") {
+            element.style.transform = `translateX(${translate}px)`;
+        } else if (direction === "vertical") {
+            element.style.transform = `translateY(${translate}px)`;
+        }
+    }
+
+    useEffect(() => {
+        if (parent.current) {
+            document.addEventListener("scroll", () => {
+                const currentScroll = window.scrollY > maxScroll ? maxScroll : window.scrollY;
+                const translate = currentScroll * rate;
+                translateElement(parent.current!, direction, translate);
+            });
+        }
+    }, []);
+
+    return <div className="transition-transform ease-out duration-300" ref={parent}>{children}</div>;
+}
