18ef22cc9b3598cb3d6a70e45112aec1e7c9fcb1
Keo Ponleou Sok <ponleousk@gmail.com>
2025-06-26 17:31:15 +1000

    feat: added line orientation in lined component

--- src/components/Lined.tsx
+++ src/components/Lined.tsx
@@ -6,29 +6,33 @@ type Orientation = (typeof orientations)[number];
 export default function Lined({
     children,
     orientation,
+    lineOrientation = orientation,
     lengthRem,
+    gapRem,
     enable = { start: true, end: true },
 }: {
     children: ReactNode;
     orientation: Orientation;
+    lineOrientation?: Orientation;
     lengthRem: number;
+    gapRem: number;
     enable?: Partial<{ start: boolean; end: boolean }>;
 }) {
     return (
-        <>
+        <div className={`flex items-center ${orientation === "vertical" ? "flex-col" : ""}`} style={{ gap: `${gapRem}rem` }}>
             {enable.start && (
                 <div
-                    style={orientation === "vertical" ? { height: `${lengthRem}rem` } : { width: `${lengthRem}rem` }}
-                    className={`${orientation === "vertical" ? "w-[1px]" : "h-[1px]"} bg-primary`}
+                    style={lineOrientation === "vertical" ? { height: `${lengthRem}rem` } : { width: `${lengthRem}rem` }}
+                    className={`${lineOrientation === "vertical" ? "w-[1px]" : "h-[1px]"} bg-primary`}
                 ></div>
             )}
             {children}
             {enable.end && (
                 <div
-                    style={orientation === "vertical" ? { height: `${lengthRem}rem` } : { width: `${lengthRem}rem` }}
-                    className={`${orientation === "vertical" ? "w-[1px]" : "h-[1px]"} bg-primary`}
+                    style={lineOrientation === "vertical" ? { height: `${lengthRem}rem` } : { width: `${lengthRem}rem` }}
+                    className={`${lineOrientation === "vertical" ? "w-[1px]" : "h-[1px]"} bg-primary`}
                 ></div>
             )}
-        </>
+        </div>
     );
 }
