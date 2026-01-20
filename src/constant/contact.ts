6212f1f63aa81b75f8423b2f2d8ebaed415fbc7d
ponleou <dev.ponleousk@gmail.com>
2026-01-20 15:49:21 +1100

    feat: about me page

---
+++ src/constant/contact.ts
@@ -0,0 +1,8 @@
+const contact = {
+    github: "https://github.com/ponleou",
+    codeberg: "https://codeberg.org/ponleou",
+    email: "dev.ponleousk@gmail.com",
+    linkedin: "https://www.linkedin.com/in/ponleou/",
+};
+
+export default contact;
