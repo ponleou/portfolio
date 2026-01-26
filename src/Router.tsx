12d788aa9fc1a6fa6507fd8451d8b5da92435ab1
ponleou <dev.ponleousk@gmail.com>
2026-01-27 00:41:09 +1100

    feat: added experience page

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes } from "react-router";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import Experience from "./pages/Experience";
 
 function Router() {
     return (
@@ -10,6 +11,7 @@ function Router() {
             <Routes>
                 <Route path="/" element={<Home />}>
                     <Route path="about" element={<About />} />
+                    <Route path="experience" element={<Experience />} />
                     <Route path="*" element={<NotFound />} />
                 </Route>
             </Routes>
