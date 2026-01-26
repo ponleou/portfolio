ef6239babd4d73002d4992906ed45eab71220fbe
ponleou <dev.ponleousk@gmail.com>
2026-01-27 00:41:09 +1100

    feat: added experience page

---
+++ src/components/Timeline.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode } from "react";
+
+export default function Timeline({
+    children,
+    start = false,
+    end = false,
+}: {
+    children: ReactNode;
+    start?: boolean;
+    end?: boolean;
+}) {
+    return (
+        <div className={`flex gap-12 ${start && "mt-8"}`}>
+            <div className="flex flex-col items-center gap-6">
+                {start && <div className={`bg-accent w-px h-12 -mt-6 -mb-12`}></div>}
+                <div className="bg-accent text-base-ad rounded-full w-4 h-4 mt-6"></div>
+                <div className={`bg-accent w-px grow ${!end && "-mb-6"}`}></div>
+            </div>
+            <div className={`grow text-primary ${!end && "mb-12"}`}>{children}</div>
+        </div>
+    );
+}
