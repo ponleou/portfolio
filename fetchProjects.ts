36aab132f9bd232be01882f8f0add37b7e1247a1
ponleou <dev.ponleousk@gmail.com>
2026-01-27 22:08:33 +1100

    fix: changed on-load fetching to on-build fetching for project data

---
+++ fetchProjects.ts
@@ -0,0 +1,22 @@
+import fs from "fs";
+import axios from "axios";
+import { projectList, apiList } from "./src/content/projects_source";
+
+async function fetchProjects() {
+    const projects: Record<string, object> = {};
+
+    for (const [index, api] of apiList.entries()) {
+        for (const projectRepo of projectList) {
+            try {
+                const response = await axios.get(api + projectRepo);
+                projects[projectRepo] = response.data;
+            } catch (error) {
+                if (!(error instanceof Error)) return;
+                console.error(error.message);
+            }
+        }
+        fs.writeFileSync(`src/data/projects-${index}.json`, JSON.stringify(projects, null, 4));
+    }
+}
+
+fetchProjects();
