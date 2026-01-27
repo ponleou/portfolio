a88dc2a77f262d53862d5b4742bc5f16ec6016c0
ponleou <dev.ponleousk@gmail.com>
2026-01-27 22:08:33 +1100

    fix: changed on-load fetching to on-build fetching for project data

---
+++ src/content/projects_source.ts
@@ -0,0 +1,12 @@
+export const projectList: Array<string> = [
+    "ponleou/pipetron",
+    "ponleou/portfolio",
+    "ponleou/dotfiles",
+    "ponleou/rockscale",
+    "ponleou/EcoRound",
+    "ponleou/Audio-Visualiser",
+    "ponleou/Find-The-Fake-Game",
+    "ponleou/Intrusion-Detection-System",
+];
+
+export const apiList = ["https://codeberg.org/api/v1/repos/", "https://api.github.com/repos/"];
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Router from "./Router.tsx";
-import { ProjectsProvider } from "./context/Projects.tsx";
 
 createRoot(document.getElementById("root")!).render(
     // <StrictMode>
-    <ProjectsProvider>
-        <Router />
-    </ProjectsProvider>,
+    <Router />,
     // </StrictMode>,
 );
