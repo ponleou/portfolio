ce4aa2bea8910a0d97e27618dbee129bbdab3b52
ponleou <dev.ponleousk@gmail.com>
2026-01-30 21:21:19 +1100

    feat: experience page shortened

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -10,13 +10,13 @@ export default function Timeline({
     end?: boolean;
 }) {
     return (
-        <div className={`flex gap-12 ${start && "mt-8"}`}>
+        <div className={`flex gap-8 ${start && "mt-4"}`}>
             <div className="flex flex-col items-center gap-6">
-                {start && <div className={`bg-accent w-px h-12 -mt-6 -mb-12`}></div>}
+                {start && <div className={`bg-accent w-px h-8 -mt-4 -mb-12`}></div>}
                 <div className="bg-accent text-base-ad rounded-full w-4 h-4 mt-6"></div>
                 <div className={`bg-accent w-px grow ${!end && "-mb-6"}`}></div>
             </div>
-            <div className={`grow text-primary ${!end && "mb-12"}`}>{children}</div>
+            <div className={`grow text-primary ${!end && "mb-10"}`}>{children}</div>
         </div>
     );
 }
