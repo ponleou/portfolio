a2c61c10eb4c0c381b1f192d1011681f53e44ec4
ponleou <dev.ponleousk@gmail.com>
2026-01-31 19:56:28 +1100

    fix: improve mobile top navbar hover state

--- src/components/Navigators.tsx
+++ src/components/Navigators.tsx
@@ -1,9 +1,16 @@
 import { NavLink } from "react-router";
 import TextSh from "./TextSh";
+import type { MouseEventHandler } from "react";
 
 type Route = {
     to: string;
     hash: string;
+};
+
+type ActiveState = {
+    matchHash: string;
+    navLinkClass?: string;
+    navClass?: string;
 };
 
 export default function Navigators({
@@ -11,17 +18,31 @@ export default function Navigators({
     routes,
     navClassName = "",
     navLinkClassName = "",
+    onClick = () => {},
+    active,
 }: {
     className?: string;
     routes: Array<Route>;
     navClassName?: string;
     navLinkClassName?: string;
+    onClick?: MouseEventHandler<HTMLAnchorElement>;
+    active?: ActiveState;
 }) {
     return (
         <div className={`${className}`}>
             {routes.map((route, index) => (
-                <NavLink key={index} className={`${navLinkClassName}`} to={`/${route.to}`} state={{ hash: route.hash }}>
-                    <TextSh className={`${navClassName}`}>{route.hash}</TextSh>
+                <NavLink
+                    onClick={onClick}
+                    key={index}
+                    className={`${navLinkClassName} ${active && active.matchHash === route.hash && active.navLinkClass}`}
+                    to={`/${route.to}`}
+                    state={{ hash: route.hash }}
+                >
+                    <TextSh
+                        className={`${navClassName} ${active && active.matchHash === route.hash && active.navClass}`}
+                    >
+                        {route.hash}
+                    </TextSh>
                 </NavLink>
             ))}
         </div>
