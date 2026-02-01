3c0115a29cd249baf1ccb0fadab6011634befd45
ponleou <dev.ponleousk@gmail.com>
2026-02-01 22:34:30 +1100

    fix: adjustments to experience page

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -12,9 +12,11 @@ export default function Timeline({
     return (
         <div className={`flex gap-8 ${start && "mt-4"}`}>
             <div className="flex flex-col items-center gap-6">
-                {start && <div className={`bg-accent/80 w-px h-8 -mt-4 -mb-12 top-fade-[75%]`}></div>}
-                <div className="bg-accent/80 text-base-ad rounded-full w-4 h-4 mt-6"></div>
-                <div className={`bg-accent/80 w-px grow ${!end ? "-mb-6 top-fade-[15%]" : "bottom-fade-[75%]"}`}></div>
+                {start && <div className={`bg-accent/80 w-px h-14 -mt-4 -mb-16 top-fade-[100%]`}></div>}
+                <div className="bg-accent/80 text-base-ad rounded-full w-4 h-4 mt-10"></div>
+                <div
+                    className={`bg-accent/80 w-px grow ${!end ? "-mb-10 top-fade-[10%]" : "bottom-fade-[100%]"}`}
+                ></div>
             </div>
             <div className={`grow ${!end && "mb-10"}`}>{children}</div>
         </div>
